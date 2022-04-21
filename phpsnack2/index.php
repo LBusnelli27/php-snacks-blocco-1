c110b1e3a58c75dfa8bef9febab0b8173f70e918
Busna27 <lucabusnelli2001@gmail.com>
2022-04-21 12:30:12 +0200

    snack 2  - done

--- phpsnack2/index.php
+++ phpsnack2/index.php
@@ -1,31 +1,31 @@
 <?php
-$ads = [
-    [
-        'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
-        'link' => 'https://www.google.com',
-        'is_active' => true,
-    ],
-    [
-        'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
-        'link' => 'https://www.facebook.com',
-        'is_active' => true,
-    ],
-    [
-        'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
-        'link' => 'https://codepen.io',
-        'is_active' => false,
-    ],
-    [
-        'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
-        'link' => 'https://laravel.com',
-        'is_active' => false,
-    ],
-    [
-        'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
-        'link' => 'https://www.php.net',
-        'is_active' => true,
-    ],
-];
+    $ads = [
+        [
+            'image_path' => 'https://conversionsciences.com/wp-content/uploads/2019/04/example-of-emotional-logical-appeal-on-persuasive-copy.jpg',
+            'link' => 'https://www.google.com',
+            'is_active' => true,
+        ],
+        [
+            'image_path' => 'https://previews.123rf.com/images/hstrongart/hstrongart1612/hstrongart161200121/68060446-anuncios-de-cepillos-de-dientes-el%C3%A9ctricos-modo-diferente-de-este-producto-con-blanco-modelo-de-diente.jpg',
+            'link' => 'https://www.facebook.com',
+            'is_active' => true,
+        ],
+        [
+            'image_path' => 'https://thumbs.dreamstime.com/z/back-to-school-sale-background-chalkboard-sale-percentages-marketing-poster-color-pencils-96751666.jpg',
+            'link' => 'https://codepen.io',
+            'is_active' => false,
+        ],
+        [
+            'image_path' => 'https://foodsecurityfoodjustice.files.wordpress.com/2016/11/beyond-the-omlette-1.jpg',
+            'link' => 'https://laravel.com',
+            'is_active' => false,
+        ],
+        [
+            'image_path' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRniLfGdFLxVOUoEwYC9WeRIrfZInB74_80IV8yMnANx3HTZYoQ',
+            'link' => 'https://www.php.net',
+            'is_active' => true,
+        ],
+    ];
 ?>
 
 <!DOCTYPE html>
@@ -40,9 +40,12 @@ $ads = [
     <?php 
         foreach ($ads as $key => $value) {
             if ($value['is_active'] == true) {
-                echo '<p><img src="' . $value['image_path'] . '" alt="Image" height="200" width="200" /></p>';
+                $activeTrue[] = $value;
             }
         }
+        $randomAd = $activeTrue[rand(0, count($activeTrue)-1)];
+
+        echo '<p><img src="' . $randomAd['image_path'] . '" alt="Random Ad" height="600" width="600" /></p>';
     ?>
 </body>
 </html>
